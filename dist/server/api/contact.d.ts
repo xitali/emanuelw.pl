ed9e30bc572d3bc8c4e1fb45bf304a5cde8948b8
xitali <emanuel.wloch@icloud.com>
2025-07-07 13:55:14 +0200

    changes

--- dist/server/api/contact.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response } from 'express';
-export default function handler(req: Request, res: Response): Promise<void>;
-//# sourceMappingURL=contact.d.ts.map
--- dist/types/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Global types for the portfolio website
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=index.js.map
