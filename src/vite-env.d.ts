7385287bfc4a2a337087968b5df5560de840c9a0
Emanuel Włoch <emanuel.wloch@icloud.com>
2026-04-08 19:19:16 +0200

    update content and navigation

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TURSO_DB_URL: string
-  readonly VITE_TURSO_AUTH_TOKEN: string
+  readonly VITE_TURSO_DB_URL?: string
+  readonly VITE_TURSO_AUTH_TOKEN?: string
 }
 
 interface ImportMeta {
