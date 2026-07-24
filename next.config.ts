685c66e6490294bf795f7a5383e0fdd54519838a
Emanuel <emanuel.wloch@icloud.com>
2026-07-24 21:05:22 +0200

    fix: dodanie dozwolonych domen do next/image dla projektów

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image2url.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-fa4842ac95b845ba9f2a3b9aa7bf24ae.r2.dev',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
